d0a61a319d24ff89cb80e970f515c0a0a3908dcd
Andrey <folganoid@yahoo.com>
2018-06-16 19:45:25 +0300

    (fnc) makeup auth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 import './index.css';
 
 const name = 'Josh Perez';
@@ -15,43 +17,97 @@ class NameForm extends React.Component {
         super(props);
         this.state = {
             login: '',
-            pass: ''
+            pass: '',
+            user: '',
+            userId: 0,
         };
 
         this.loginChange = this.loginChange.bind(this);
         this.passChange = this.passChange.bind(this);
+        this.userChange = this.userChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.logOut = this.logOut.bind(this);
     }
 
     loginChange(event) {
         this.setState({login: event.target.value});
     }
+
     passChange(event) {
         this.setState({pass: event.target.value});
     }
 
+    logOut() {
+        this.setState({user: ''});
+        this.setState({userId: 0});
+    }
+
+    userChange(user, userId) {
+        this.setState({user: user});
+        this.setState({userId: userId});
+    }
+
+
     handleSubmit(event) {
-        alert(this.state.login + ' ' + this.state.pass);
+
+        var formData = new FormData();
+        formData.append('login', this.state.login);
+        formData.append('pass', this.state.pass);
+
+        axios({
+            method: 'post',
+            url: 'http://127.0.0.1:3001/user',
+            data: formData,
+
+            config: {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Origin': 'http://localhost:3000',
+                }
+            },
+
+        }).then(function (response) {
+            console.log(response);
+            if (response.data.Id != 0) {
+                this.userChange(response.data.Login, response.data.Id);
+            }
+        }.bind(this));
+
         event.preventDefault();
     }
 
     render() {
+
+        let login;
+
+        if (this.state.userId == 0) {
+            login = <ul className="uk-navbar-nav">
+                <li className="uk-navbar-content ifLogout">
+                    <input name="login" type="text" size="5" placeholder="login" onChange={this.loginChange}/>
+                    <input name="pass" type="password" size="5" placeholder="pass" onChange={this.passChange}/>
+                </li>
+                <li className="uk-navbar-content ifLogout">
+                    <button type="submit">Login</button>
+                </li>
+            </ul>;
+        } else {
+            login = <ul className="uk-navbar-nav">
+                <li className="uk-navbar-content ifLogin">
+                    <button className="uk-button" type="reset" onClick={this.logOut}>Logout</button>
+                </li>
+                <li className="uk-navbar-content ifLogin"><b>{this.state.user}</b></li>
+            </ul>;
+        }
+
         return (
             <form onSubmit={this.handleSubmit}>
-                <ul className="uk-navbar-nav">
-                    <li className="uk-navbar-content">
-                        <input name="login" type="text" size="5" placeholder="login" onChange={this.loginChange}/>
-                        <input name="pass" type="password" size="5" placeholder="pass" onChange={this.passChange}/>
-                    </li>
-                    <li className="uk-navbar-content"><button type="submit">Login</button></li>
-                    <li className="uk-navbar-content">User</li>
-                </ul>
+                {login}
             </form>
         );
     }
 }
 
 ReactDOM.render(
-    <NameForm />,
+    <NameForm/>,
     document.getElementById('auth_form')
 );
