d9f4a7f1144543121e4fc177729290b4d7404296
Andrey <folganoid@yahoo.com>
2018-08-18 23:06:30 +0300

    (fnc)(rfc) CRUD-REST year, bike, yeardist

--- src/pages/data.jsx
+++ src/pages/data.jsx
@@ -21,6 +21,8 @@ class Data extends React.Component {
         this.fillLists = this.fillLists.bind(this);
         this.saveBikeAjax = this.saveBikeAjax.bind(this);
         this.saveTireAjax = this.saveTireAjax.bind(this);
+        this.saveYearDist = this.saveYearDist.bind(this);
+        this.deleteYD = this.deleteYD.bind(this);
 
         this.fillLists();
     }
@@ -36,7 +38,7 @@ class Data extends React.Component {
         // get bike list
         axios({
             method: 'post',
-            url: SETUP.goHost + '/get_bike_list',
+            url: SETUP.goHost + '/bike',
             data: formData,
             config: {
                 headers: {
@@ -48,6 +50,10 @@ class Data extends React.Component {
         }).then(function (response) {
 
             that.setState({bikeList: response.data});
+            if (response.data[0].Name !== undefined) {
+                that.setState({addBike: response.data[0].Name});
+                that.setState({addYDBike: response.data[0].Name});
+            }
 
         }).catch((error) => {
             if (error.response) {
@@ -58,7 +64,7 @@ class Data extends React.Component {
         // get tire list
         axios({
             method: 'post',
-            url: SETUP.goHost + '/get_tire_list',
+            url: SETUP.goHost + '/tire',
             data: formData,
             config: {
                 headers: {
@@ -71,9 +77,35 @@ class Data extends React.Component {
 
             that.setState({tireList: response.data});
 
+            if (response.data[0].Name !== undefined) {
+                that.setState({addTire: response.data[0].Name});
+            }
+
         }).catch((error) => {
             if (error.response) {
                 that.props.done("Tire list not found!", "uk-alert-warning");
+            }
+        });
+
+        // get year-dist list
+        axios({
+            method: 'post',
+            url: SETUP.goHost + '/year_dist',
+            data: formData,
+            config: {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Origin': SETUP.reactHost,
+                }
+            },
+
+        }).then(function (response) {
+
+            that.setState({yearDistList: response.data});
+
+        }).catch((error) => {
+            if (error.response) {
+                that.props.done("Year-dist list not found!", "uk-alert-warning");
             }
         });
     }
@@ -92,6 +124,10 @@ class Data extends React.Component {
         });
     }
 
+    /**
+     * [saveBikeAjax description]
+     * @return {[type]} [description]
+     */
     saveBikeAjax() {
 
         let formData = new FormData();
@@ -102,8 +138,8 @@ class Data extends React.Component {
         let that = this;
 
         axios({
-            method: 'post',
-            url: SETUP.goHost + '/add_bike',
+            method: 'PUT',
+            url: SETUP.goHost + '/bike',
             data: formData,
             config: {
                 headers: {
@@ -121,6 +157,10 @@ class Data extends React.Component {
         });
     }
 
+    /**
+     * [saveTireAjax description]
+     * @return {[type]} [description]
+     */
     saveTireAjax() {
         
         let formData = new FormData();
@@ -131,8 +171,8 @@ class Data extends React.Component {
         let that = this;
 
         axios({
-            method: 'post',
-            url: SETUP.goHost + '/add_tire',
+            method: 'PUT',
+            url: SETUP.goHost + '/tire',
             data: formData,
             config: {
                 headers: {
@@ -151,10 +191,92 @@ class Data extends React.Component {
     }
 
     /**
+     * [saveYearDist description]
+     * @return {[type]} [description]
+     */
+    saveYearDist() {
+        
+        let formData = new FormData();
+        formData.append('userid', this.props.state.userId);
+        formData.append('token', this.props.state.token);
+        formData.append('year', this.state.addYDYear);
+        formData.append('bike', this.state.addYDBike);
+        formData.append('dist', this.state.addYDDist);
+
+        let that = this;
+
+        axios({
+            method: 'PUT',
+            url: SETUP.goHost + '/year_dist',
+            data: formData,
+            config: {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Origin': SETUP.reactHost,
+                }
+            },
+
+        }).then(function (response) {
+            that.fillLists();
+        }).catch((error) => {
+            if (error.response) {
+                that.props.done("Error! Can't save tike.", "uk-alert-warning");
+            }
+        });
+    }
+
+    /**
+     * [deleteYD description]
+     * @param  {[type]} id   [description]
+     * @param  {[type]} path [description]
+     * @return {[type]}      [description]
+     */
+    deleteYD(id, path) {
+        
+        let formData = new FormData();
+        formData.append('userid', this.props.state.userId);
+        formData.append('token', this.props.state.token);
+        formData.append('id', id);
+
+        let that = this;
+
+        axios({
+            method: 'DELETE',
+            url: SETUP.goHost + path,
+            data: formData,
+            config: {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Origin': SETUP.reactHost,
+                }
+            },
+
+        }).then(function (response) {
+            that.fillLists();
+        }).catch((error) => {
+            if (error.response) {
+                that.props.done("Error! Can't save tike.", "uk-alert-warning");
+            }
+        });
+    }
+
+     /**
      * render
      * @returns {*}
      */
     render() {
+
+    let delYearDist = (event) => {
+        this.deleteYD(event.target.value, "/year_dist");
+    }
+
+    let delTire = (event) => {
+        this.deleteYD(event.target.value, "/tire");
+    }
+
+    let delBike = (event) => {
+        this.deleteYD(event.target.value, "/bike");
+    }
 
         return (
             <div className="uk-container">
@@ -302,24 +424,23 @@ class Data extends React.Component {
                             <button className="uk-button uk-button-primary" id="save_bike" type="button" onClick={this.saveBikeAjax}>Save bike</button>
                             <br />
                             {this.state.bikeList.map(function(val, index){
-                                return <dd key={ index }>{val.Name}</dd>;
+                                return <dd key={ index }>{val.Name}<button value={val.Id} onClick={delBike}>X</button></dd>;
                             })}
                         <h3>Add tires</h3>
                             <input id="add_tire" type="text" name="addTire" placeholder="Enter tire" onChange={this.handleInputChange}/>
                              <button className="uk-button uk-button-primary" id="save_tire" type="button" onClick={this.saveTireAjax}>Save tire</button>
                             <br />
                             {this.state.tireList.map(function(val, index){
-                                return <dd key={ index }>{val.Name}</dd>;
+                                return <dd key={ index }>{val.Name} <button value={val.Id} onClick={delTire}>X</button></dd>;
                             })}
 
                         <h3>Add year distantion</h3>
-                            <form>
                             <table>
                                 <tbody>
                                     <tr>
                                         <td>Bike:</td>
                                         <td>
-                                            <select name="bike">
+                                            <select name="addYDBike" onChange={this.handleInputChange}>
                                                 {this.state.bikeList.map(function(val, index){
                                                     return <option key={ index } value={val.Name}>{val.Name}</option>;
                                                 })}
@@ -329,15 +450,20 @@ class Data extends React.Component {
                                     <tr>
                                         <td>Year / Dist </td>
                                         <td>
-                                            <input type="text" name="odoyear"/>/
-                                            <input type="text" name="ododist"/>
-                                            <input type="submit" value="Save" />
+                                            <input type="text" name="addYDYear" onChange={this.handleInputChange} />/
+                                            <input type="text" name="addYDDist" onChange={this.handleInputChange} />
+                                            <button className="uk-button uk-button-primary" type="button" onClick={this.saveYearDist}>Save</button>
                                         </td>
                                     </tr>
                                 </tbody>
                             </table>
-                            </form>        
-
+                            <table>
+                                <tbody>
+                                    {this.state.yearDistList.map(function(val, index){
+                                        return <tr key={ index }><td>{val.Year}</td><td>{val.Bike}</td><td>{val.Dist}</td><td><button value={val.Id} onClick={delYearDist}>X</button></td></tr>;
+                                    })}
+                                </tbody>
+                            </table>
                     </div>
                 </div>
                 <br />
